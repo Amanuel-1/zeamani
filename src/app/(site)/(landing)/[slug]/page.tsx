a461e714de6a42e2e85a2bb36891ab6e8cca48bc
Amanuel-1 <amanuelgaromsa@gmail.com>
2023-11-11 23:25:34 +0300

    changed a bit

--- src/app/(site)/(landing)/[slug]/page.tsx
+++ src/app/(site)/(landing)/[slug]/page.tsx
@@ -49,7 +49,7 @@ const PostPage = async ({params:{slug}}:postProps) => {
             <PortableTextEditor body={post.body}/>
            </div>
 
-            <LikeButton post={post}/>
+            <LikeButton slug={post.slug.current}/>
         
     </section>
   )
