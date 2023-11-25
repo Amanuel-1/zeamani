ee8d440c912a6a3bf3de24baa7045e6919f1fb34
Amanuel-1 <amanuelgaromsa@gmail.com>
2023-11-25 15:53:55 +0300

    modified the og images

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -1,51 +1,135 @@
-import { Me } from "fireup/lib/constants";
-import { urlForImage } from "fireup/lib/image";
 import { getDomain } from "fireup/lib/utils";
-import { ImageResponse, NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+import { ImageResponse } from "@vercel/og";
+import { urlForImage } from "fireup/lib/image";
 
-export const runtime ='edge'
 
+export async function GET(req: NextRequest) {
+const params = req.nextUrl.searchParams;
+const author = params.get('author');
+const title = params.get('title');
+const image = params.get('image');
+const avatar = params.get('avatar');
 
-export  async function GET(req:NextRequest){
+const website = getDomain()
+return new ImageResponse(
+    <div
+    style={{
+        position: 'relative',
+        display: 'flex',
+        flexDirection: 'column',
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#1F2937',
+    }}
+    >
+    <div
+        style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        backgroundColor: '#F3F4F6',
+        display: 'flex',
+        width: '100%',
+        height: '100%',
+        }}
+    >
+        <img
+        height="400"
+        width="399"
+        style={{
+            height: '100%',
+            width: '100%',
+            zIndex: 20,
+        }}
+        src={image??"https://user-images.githubusercontent.com/71079278/275351091-bf163aa6-a4d0-4022-9ef4-917e0e1bcfb2.png"}
+        alt="Post cover image"
+        />
+    </div>
+    <div
+        style={{
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        width: '100%',
+        height: '100%',
+        borderTopLeftRadius: '50%',
+        background: 'linear-gradient(to right top, #C05621, rgba(30, 30, 30, 0.5))',
+        transform: 'skewY(25deg)',
+        backdropFilter: 'blur(30px)',
+        }}
+    ></div>
 
-    const params  = req.nextUrl.searchParams
-    const author  = params.get('author')
-    const title   = params.get('title')
-    const image  = params.get('image')
-    const avatar  = params.get('avatar')
-    const website  = getDomain()
-    return new ImageResponse(
-    (
-        <div tw=" absolute flex flex-col w-full h-full items-center justify-center bg-stone-800">
-        <div tw=" absolute top-0 left-0 rounded-r-full bg-gray-50 flex w-[50%] bg-amber-700 h-full">
-            <img height="400" width={399} style={{
-                height:"100%",
-                width:"100%",
-                zIndex:20
-            }} src={image??"https://user-images.githubusercontent.com/71079278/275351091-bf163aa6-a4d0-4022-9ef4-917e0e1bcfb2.png"} tw="rounded-br-full" alt="Post background image" />
-            <div tw="flex flex-col w-full justify-center items-center">
-                <div tw="flex flex-col min-h-screen bg-transparent relative flex-wrap justify-center items-center px-4 text-wrap">
-                    <span tw="flex flex-wrap text-wrap  text-4xl sm:text-5xl lg:text-6xl font-bold text-stone-100 ">{title}</span>
-                    <span tw="text-stone-300 w-full text-lg sm:text-xl md:text-2xl  py-3 font-extrabold">
-                    {website}</span>
-                    
-                    <div tw="absolute bottom-6 left-6 text-stone-500 flex gap-4">
-                    <span tw=" rounded-full h-[5rem] w-[5rem]  bg-blue-400 overflow-hidden border-4 border-neutral-100">
-                        <img src={avatar as string ?? "https://media.licdn.com/dms/image/D4D03AQFhYXOlndUMyA/profile-displayphoto-shrink_200_200/0/1689808459339?e=1706140800&v=beta&t=B8314U4DPcI2tcYX6MHTCqBfbQ3v681G8YP_hNx0Gvg"} tw="h-full w-full border-3 rounded-full " />
-                    </span>
-                     <span tw="text-stone-500 px-4  text-center self-center font-bold  text-xl text-amber-300 ">Amanuel Garomsa  [author]</span>
-                     </div>
-                   
-                </div>
-                
-               
+    <div
+        style={{
+        position: 'absolute',
+        top: '1rem',
+        left: '1rem',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '-2rem',
+        width: '100%',
+        height: '100%',
+        color: 'white',
+        fontWeight: 'bold',
+        }}
+    >
+        <h1 tw="font-extrabold" style={{ width: '60%', fontSize: '4rem' }}>{title??"Untitled Post"}</h1>
+        <h1  style={{ display:'flex',justifyContent:'center',textAlign:'center',minWidth:'25%',maxWidth:'35%',borderRadius:'50px',fontSize: '2rem' ,paddingLeft:'.8rem',paddingRight:'.8rem',paddingTop:'.6rem',paddingBottom:'.6rem',backgroundColor:'rgba(40,30,30,.8)'}}>{website}</h1>
+    </div>
+        <div
+            style={{
+            position: 'absolute',
+            top: '0rem',
+            left: '0rem',
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '-2rem',
+            width: '100%',
+            height: '100%',
+            color: 'white',
+            fontWeight: 'bold',
+            
+            }}
+        >
+        <div style={{
+            width:'100%',
 
-               
+            height:'100%',
+            position:'relative',
+            display:'flex'
+            
+        }} tw="">
+
+            <div
+            style={{
+                position: 'absolute',
+                bottom: '2rem',
+                left: '60%',
+                transform: 'translateX(-50%)',
+                display: 'flex',
+                alignItems: 'center',
+                gap: '.5rem',
+                color: 'white',
+                fontWeight: 'bold',
+            }}
+            >
+            <span style={{ marginBottom: '.5rem', borderRadius: '50%' }}>
+                <img
+                src={avatar as string ?? 'https://media.licdn.com/dms/image/D4D03AQFhYXOlndUMyA/profile-displayphoto-shrink_200_200/0/1689808459339?e=1706140800&v=beta&t=B8314U4DPcI2tcYX6MHTCqBfbQ3v681G8YP_hNx0Gvg'}
+                style={{ height: '5rem', width: '5rem', borderRadius: '50%', border: '4px solid #F3F4F6' }}
+                />
+            </span>
+            <span style={{ color: '#F3F4F6', fontSize: '1.5rem' }}>{author??"Amanuel Garomsa "}[author]</span>
             </div>
-        </div>
-        </div>
-    )
-    
-    )
 
+        </div>
+        </div>
+    </div>
+);
 }
+
+
+
