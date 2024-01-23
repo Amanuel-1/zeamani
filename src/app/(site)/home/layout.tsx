8a710b69f6f149a8e295d781816686cacb855e8f
Amanuel-1 <amanuelgaromsa@gmail.com>
2024-01-24 01:19:03 +0300

    fix2

--- src/app/(site)/home/layout.tsx
+++ src/app/(site)/home/layout.tsx
@@ -36,7 +36,6 @@ export default function RootLayout({
         <TRPCReactProvider headers={headers()}>
           
          <SessionProvide>
-         <Navbar/>
          <Analytics/>
           {children}
           <Footer/>
