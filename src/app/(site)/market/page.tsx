982415303bbd10bba107e93ef5f87401afcc9e44
Amanuel-1 <amanuelgaromsa@gmail.com>
2024-01-31 21:25:14 +0100

    fix 3

---
+++ src/app/(site)/market/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import UnderConstfuction from '../underConstruciton/page'
+
+const Market = () => {
+  return (
+    <UnderConstfuction/>
+  )
+}
+
+export default Market
