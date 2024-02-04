4ddeef730a62594ce512cd7506a93f4eb0497ff8
Amanuel-1 <amanuelgaromsa@gmail.com>
2024-02-04 22:37:38 +0300

    fix2

--- src/app/(site)/articles/layout.tsx
+++ src/app/(site)/articles/layout.tsx
@@ -25,37 +25,7 @@ const inter = Inter({
 });
 
 
-export async function generateMetadata(
-  parent: ResolvingMetadata
-): Promise<Metadata> {
-  // read route params
 
- 
-  // fetch data
-  const title =  "Zeamani Articles";
- 
-
-  const website  = getDomain()
-  // optionally access and extend (rather than replace) parent metadata
-  const previousImages = (await parent).openGraph?.images ?? []
- 
-  return {
-    title: title,
-    description:"Explore insightful articles on www.zeamani.com covering a wide range of topics. From tech newd to liestyle, my articles provide valuable information and perspectives. Join me for engaging reads that inform and inspire.",
-    authors:[{name:"Amanuel Garomsa",url:`https://www.linkedin.com/in/amanuel-garomsa-36ba79213/`}],
-    creator:"Amauel Garomsa",
-    keywords:["blog","nature","technology","crypto","data mining","art","love","football","lifestyle"],
-    openGraph:{
-      images: ["https://images4.imagebam.com/3f/9d/09/MERRWNC_o.png"],
-    },
-    twitter: {
-      card: "summary_large_image",
-      title: title,
-      description:"Explore insightful articles on www.zeamani.com covering a wide range of topics. From [briefly mention key topics/niches you cover] to [highlight another key aspect], our articles provide valuable information and perspectives. Join us for engaging reads that inform and inspire.",
-      images: ["https://images4.imagebam.com/3f/9d/09/MERRWNC_o.png"],
-    }
-  }
-}
 
 export default function RootLayout({
   children,
--- src/app/(site)/articles/page.tsx
+++ src/app/(site)/articles/page.tsx
@@ -5,10 +5,44 @@ import SearchBar from 'fireup/app/_components/shared/contents/SearchBar'
 import PostsList from 'fireup/app/_components/shared/contents/postsList'
 import { client, sanityFetch } from 'fireup/lib/client'
 import { SPost } from 'fireup/lib/types'
+import { getDomain } from 'fireup/lib/utils'
 import post from 'fireup/schemas/post'
 import { dancing, pacifico } from 'fireup/styles/Fonts'
 import { groq } from 'next-sanity'
+import { Metadata, ResolvingMetadata } from 'next/types'
 import React from 'react'
+
+export async function generateMetadata(
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+
+ 
+  // fetch data
+  const title =  "Zeamani Articles";
+ 
+
+  const website  = getDomain()
+  // optionally access and extend (rather than replace) parent metadata
+  const previousImages = (await parent).openGraph?.images ?? []
+ 
+  return {
+    title: title,
+    description:"Explore insightful articles on www.zeamani.com covering a wide range of topics. From tech newd to liestyle, my articles provide valuable information and perspectives. Join me for engaging reads that inform and inspire.",
+    authors:[{name:"Amanuel Garomsa",url:`https://www.linkedin.com/in/amanuel-garomsa-36ba79213/`}],
+    creator:"Amauel Garomsa",
+    keywords:["blog","nature","technology","crypto","data mining","art","love","football","lifestyle"],
+    openGraph:{
+      images: ["https://images4.imagebam.com/3f/9d/09/MERRWNC_o.png"],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: title,
+      description:"Explore insightful articles on www.zeamani.com covering a wide range of topics. From [briefly mention key topics/niches you cover] to [highlight another key aspect], our articles provide valuable information and perspectives. Join us for engaging reads that inform and inspire.",
+      images: ["https://images4.imagebam.com/3f/9d/09/MERRWNC_o.png"],
+    }
+  }
+}
 
 const Articles = async() => {
 
