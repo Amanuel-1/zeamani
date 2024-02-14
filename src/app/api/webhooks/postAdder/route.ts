bd994138bfbbd18fc1d42516a5a4c6723b1265c1
Amanuel-1 <amanuelgaromsa@gmail.com>
2024-02-14 05:12:33 +0300

    aa

--- src/app/api/webhooks/postAdder/route.ts
+++ src/app/api/webhooks/postAdder/route.ts
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 
 const secret = "psupabasePostCreateBook"
 
-export default async function handler(req:NextApiRequest, res:NextApiResponse) {
+export default async function POST(req:NextApiRequest, res:NextApiResponse) {
   const signature = req.headers[SIGNATURE_HEADER_NAME]
   const body = await readBody(req) // Read the body into a string
   if (!(await isValidSignature(body, signature as any, secret))) {
