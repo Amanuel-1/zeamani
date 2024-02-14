1199fb9abc58b4149b163c6859d55c1ed65debd0
Amanuel-1 <amanuelgaromsa@gmail.com>
2024-02-14 05:56:11 +0300

    sfdgsdfggsdgsdfgs

--- src/app/api/webhooks/postAdder/route.ts
+++ src/app/api/webhooks/postAdder/route.ts
@@ -1,21 +1,23 @@
+// src/app/api/webhooks/postAdder/route.ts
+
+import { NextApiRequest, NextApiResponse } from 'next';
 import { SIGNATURE_HEADER_NAME, isValidSignature } from "@sanity/webhook";
 
-const handler = async(req:any, res:any) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const signature = req.headers[SIGNATURE_HEADER_NAME].toString()
-    if(!isValidSignature(
-      JSON.stringify(req.body),
-      signature,
-      "psupabasePostCreateBook"
-    )){
-      return res.status(401).json({msg: 'Invalid request'})
-    }
-    await res.revalidate(`/`)
-    res.status(200).json({msg: 'Revalidation successful'})
-  } catch (error) {
-    res.status(500).json({ error: error, mgs: 'Something went wrong'})
-  }
-}
+    const signature = req.headers[SIGNATURE_HEADER_NAME]?.toString();
 
-export default handler
-""
+    if (!isValidSignature(
+      JSON.stringify(req.body),
+      signature as any,
+      "psupabasePostCreateBook"
+    )) {
+      return res.status(401).json({ msg: 'Invalid request' });
+    }
+
+    await res.revalidate(`/`);
+    res.status(200).json({ msg: 'Revalidation successful' });
+  } catch (error) {
+    res.status(500).json({ error, msg: 'Something went wrong' });
+  }
+};
