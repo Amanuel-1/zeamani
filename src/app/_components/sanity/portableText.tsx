33cb68d6dd51077f48285691fed568efeaf0e0c8
Amanuel-1 <amanuelgaromsa@gmail.com>
2024-02-25 16:17:20 +0300

    fix :embed component

--- src/app/_components/sanity/portableText.tsx
+++ src/app/_components/sanity/portableText.tsx
@@ -98,9 +98,7 @@ const CodeComponent : React.FC=(props:any) => {
 
       return (
         <iframe height="300" className='w-full h-[20rem]  md:h-[30rem] md:p-4' title="Loader Animation - Pure CSS" src={props.value.url} loading="lazy" allowTransparency={true} allowFullScreen={true}>
-          See the Pen <a href="https://codepen.io/dui77/pen/WNmqjRN">
-          Loader Animation - Pure CSS</a> by Susan Labude (<a href="https://codepen.io/dui77">@dui77</a>)
-          on <a href="https://codepen.io">CodePen</a>
+          
       </iframe>
       )
     }
@@ -121,7 +119,7 @@ const PortableTextComponents = {
     code: CodeComponent,
     youtube:YoutubeBlock,
     twitter:TwitterBlock,
-    embed:EmbedPreview
+    embed:EmbedBlock
 
     
   },
