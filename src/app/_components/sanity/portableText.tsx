f118c0676e3180367091b4d0b7506134c60cb679
Amanuel-1 <amanuelgaromsa@gmail.com>
2024-02-25 16:11:02 +0300

    change embed

--- src/app/_components/sanity/portableText.tsx
+++ src/app/_components/sanity/portableText.tsx
@@ -92,6 +92,19 @@ const CodeComponent : React.FC=(props:any) => {
       )
     }
 
+    const EmbedBlock:React.FC=(props: any)=>{
+      
+      
+
+      return (
+        <iframe height="300" className='w-full h-[20rem]  md:h-[30rem] md:p-4' title="Loader Animation - Pure CSS" src={props.url} loading="lazy" allowTransparency={true} allowFullScreen={true}>
+          See the Pen <a href="https://codepen.io/dui77/pen/WNmqjRN">
+          Loader Animation - Pure CSS</a> by Susan Labude (<a href="https://codepen.io/dui77">@dui77</a>)
+          on <a href="https://codepen.io">CodePen</a>
+      </iframe>
+      )
+    }
+
     
     const TwitterBlock:React.FC=(props:any)=>{
       return (
