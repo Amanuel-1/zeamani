3a9c6ff46a1d04c7424e60dffcc092990a7aeca3
Amanuel-1 <amanuelgaromsa@gmail.com>
2024-02-25 16:11:48 +0300

    feat:embed

--- src/app/_components/sanity/portableText.tsx
+++ src/app/_components/sanity/portableText.tsx
@@ -97,7 +97,7 @@ const CodeComponent : React.FC=(props:any) => {
       
 
       return (
-        <iframe height="300" className='w-full h-[20rem]  md:h-[30rem] md:p-4' title="Loader Animation - Pure CSS" src={props.url} loading="lazy" allowTransparency={true} allowFullScreen={true}>
+        <iframe height="300" className='w-full h-[20rem]  md:h-[30rem] md:p-4' title="Loader Animation - Pure CSS" src={props.value.url} loading="lazy" allowTransparency={true} allowFullScreen={true}>
           See the Pen <a href="https://codepen.io/dui77/pen/WNmqjRN">
           Loader Animation - Pure CSS</a> by Susan Labude (<a href="https://codepen.io/dui77">@dui77</a>)
           on <a href="https://codepen.io">CodePen</a>
